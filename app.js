69333d6b0e96a4b14fe6a2e17e228dc497238ab3
FraCav99 <MasterCaster36@outlook.it>
2021-02-09 19:17:53 +0100

    Add static assets, views and start building index page

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+// Set public folder for static files and view engine
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
 app.use('/', indexRouter);
 app.use('/manga', mangaRouter);
 app.use('/author', authorRouter);
