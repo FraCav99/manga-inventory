df46b232dc935c85d124627d1939e4dc71d8df33
FraCav99 <MasterCaster36@outlook.it>
2021-02-10 18:53:58 +0100

    Modify structure and stylesheets of index and add dynamic date

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+let currentYear = new Date().getFullYear();
+
 router.get('/', (req, res) => {
-    res.render('index', { title: 'Home' });
+    res.render('index', { title: 'Home', currentYear });
 });
 
 module.exports = router;
